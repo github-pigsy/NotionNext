48bc7facadf605468aef2931dac7fd3a38e83ea2
github-pigsy <40156032+github-pigsy@users.noreply.github.com>
2024-12-15 18:02:33 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -310,13 +310,15 @@ const BLOG = {
   MUSIC_PLAYER_ORDER: process.env.NEXT_PUBLIC_MUSIC_PLAYER_ORDER || 'list', // 默认播放方式，顺序 list，随机 random
   MUSIC_PLAYER_AUDIO_LIST: [
     // 示例音乐列表。除了以下配置外，还可配置歌词，具体配置项看此文档 https://aplayer.js.org/#/zh-Hans/
+    /* 这首音乐屏蔽掉
     {
       name: '陈一发-童话镇',
       artist: '陈一发',
       url: 'https://pan.mengde.org/s/EfcFLjW2JLwqzLP/download/%E9%99%88%E4%B8%80%E5%8F%91%E5%84%BF%20-%20%E7%AB%A5%E8%AF%9D%E9%95%87.mp3',
       cover:
         'https://i.scdn.co/image/ab67616d0000b27307edf9e9a545520bd0ccb737'
-    },
+    }, 
+    */
     {
       name: '纯音乐',
       artist: '纯音乐',
